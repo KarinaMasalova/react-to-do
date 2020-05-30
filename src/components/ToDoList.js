9f452ca43d5b4410db5fcc14807d04ac79700590
Karina Masalova <masalova.karina@mail.ru>
2020-05-30 20:15:48 +0300

    feat: add some basic components

---
+++ src/components/ToDoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ToDoListItem from './ToDoListItem';
+
+const ToDoList = () => {
+  return (
+    <ul>
+      <li><ToDoListItem/></li>
+    </ul>
+  );
+};
+
+export default ToDoList;
