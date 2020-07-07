4e0aec711d408ce75c926fe956cddb81b4851726
Karina Masalova <masalova.karina@mail.ru>
2020-07-07 19:55:54 +0300

    feat: add item status filter

---
+++ src/components/item-status-filter.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './item-status-filter.css';
+
+const ItemStatusFilter = () => {
+  return (
+    <div className="btn-group">
+      <button 
+        type="button"
+        className='btn btn-all'>
+      </button>
+      <button 
+        type="button"
+        className='btn btn-active'>
+      </button>
+      <button 
+        type="button"
+        className='btn btn-done'>
+      </button>
+    </div>
+  );
+};
+
+export default ItemStatusFilter;
