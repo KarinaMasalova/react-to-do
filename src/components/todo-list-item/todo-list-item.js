42f4009f74b6c5533386a8415ab4443f67e056d8
Karina Masalova <masalova.karina@mail.ru>
2020-08-31 15:03:51 +0300

    refactor: change some functions for classes

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './todo-list-item.css';
 
-const ToDoListItem = ( { label, important = false } ) => {
+export default class ToDoListItem extends Component {
+  render() {
+    const { label, important = false } = this.props;
 
-  const style = {
-    color: important ? 'steelblue' : 'black',
-    fontWeight: important ? 'bold' : 'normal'
-  };  
-
-  return (
-    <span>
-      <span className="todo-list-item-label" style={ style }>
-        { label }
+    const style = {
+      color: important ? 'steelblue' : 'black',
+      fontWeight: important ? 'bold' : 'normal'
+    };  
+  
+    return (
+      <span>
+        <span className="todo-list-item-label" style={ style }>
+          { label }
+        </span>
+        <button type="button" className="btn btn-outline-success btn-sm float-right">
+          <i className="fa fa-exclamation"></i>
+        </button>
+        <button type="button" className="btn btn-outline-danger btn-sm float-right">
+          <i className="fa fa-trash-o"></i>
+        </button>
       </span>
-      <button type="button" className="btn btn-outline-success btn-sm float-right">
-        <i className="fa fa-exclamation"></i>
-      </button>
-      <button type="button" className="btn btn-outline-danger btn-sm float-right">
-        <i className="fa fa-trash-o"></i>
-      </button>
-    </span>
-  );
-};
-
-export default ToDoListItem;
+    );
+  }
+}
