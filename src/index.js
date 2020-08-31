9ac963cdb034df882dcdc660b48b5ddf40abd5db
Karina Masalova <masalova.karina@mail.ru>
2020-08-31 13:09:36 +0300

    refactor: improve project structure

--- src/index.js
+++ src/index.js
@@ -1,30 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AppHeader from './components/app-header';
-import SearchPanel from './components/search-panel';
-import ToDoList from './components/todo-list';
-import ItemStatusFilter from './components/item-status-filter';
-
-import './index.css';
-
-const App = () => {
-  const toDoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },    
-    { label: 'Make awesome app', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 },
-  ];
-
-  return (
-    <div className='todo-app'>
-      <AppHeader toDo={1} done={3}/>
-      <div className='top-panel d-flex'>
-        <SearchPanel/>
-        <ItemStatusFilter/>
-      </div>
-      <ToDoList toDos={ toDoData }/>
-    </div>
-  );
-};
+import App from './components/app';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
